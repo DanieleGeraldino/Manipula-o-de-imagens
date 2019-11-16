c4fbe928cd9737f7fc5e776aadd061783aaf89e5
daniele <danielegeraldino13@gmail.com>
2019-11-16 09:31:48 -0300

    teste

---
+++ processamento_imagens.py
@@ -0,0 +1,37 @@
+#limite de pixes
+def limit(x):
+    if x < 0:
+        return 0
+    elif x > 255:
+        return 255
+    else:
+        return x
+
+        
+
+def convolucao(imagem, kernel):
+    # 
+    # Adicione o seu codigo aqui.
+    # 
+    return imagem
+
+
+def cinza(imagem):
+    # 
+    # Adicione o seu codigo aqui.
+    # 
+    return imagem
+
+
+def limiar(imagem, valor):
+    # 
+    # Adicione o seu codigo aqui.
+    # 
+    return imagem
+
+
+def multiplicacao(imagem, valor):
+    # 
+    # Adicione o seu codigo aqui.
+    # 
+    return imagem
